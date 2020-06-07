9fcdea6a508bee88af54dfab2a32fe8900008e13
Fernando <fsabarit@gmail.com>
2020-06-07 12:29:43 +0200

    Primer commit

---
+++ server/sockets/socket.js
@@ -0,0 +1,93 @@
+const { io } = require('../server');
+const { Usuarios } = require('../classes/usuarios');
+const { crearMensaje } = require('../utilidades/utilidades');
+
+const usuarios = new Usuarios;
+
+io.on('connection', (client) => {
+
+    client.on('entrarChat', (data, callback) => {
+
+        //console.log(data);
+
+        if (!data.nombre || !data.sala) {
+            return callback({
+                error: true,
+                mensaje: 'El nombre/sala es necesario'
+            });
+        }
+
+        client.join(data.sala);
+
+        //let personas = usuarios.agregarPersona(client.id, data.nombre, data.sala);
+        usuarios.agregarPersona(client.id, data.nombre, data.sala);
+
+        client.broadcast.to(data.sala).emit('listaPersona', usuarios.getPersonasPorSala(data.sala));
+
+        //console.log(personas);
+        console.log(usuarios.getPersonasPorSala(data.sala));
+    });
+
+    client.on('crearMensaje', (data) => {
+
+        let persona = usuarios.getPersona(client.id);
+
+        let mensaje = crearMensaje(persona.nombre, data.mensaje);
+        client.broadcast.to(persona.sala).emit('crearMensaje', mensaje);
+
+    });
+
+    client.on('disconnect', () => {
+
+        let personaBorrada = usuarios.borrarPersona(client.id);
+
+        client.broadcast.to(personaBorrada.sala).emit('crearMensaje', crearMensaje('Administrador', `${personaBorrada.nombre} salió`));
+        client.broadcast.to(personaBorrada.sala).emit('listaPersona', usuarios.getPersonasPorSala());
+    });
+
+    //Mensaje privado
+    client.on('mensajePrivado', data => {
+
+        let persona = usuarios.getPersona(client.id);
+        //Habría que comprobar que viniese la data
+        client.broadcast.to(data.para).emit('mensajePrivado', crearMensaje(persona.nombre, data.mensaje));
+
+    });
+
+    // console.log('Usuario conectado');
+
+    // client.emit('enviarMensaje', {
+    //     usuario: 'Administrador',
+    //     mensaje: 'Bienvenido a esta aplicación'
+    // });
+
+
+
+    // client.on('disconnect', () => {
+    //     console.log('Usuario desconectado');
+    // });
+
+    // // Escuchar el cliente
+    // client.on('enviarMensaje', (data, callback) => {
+
+    //     console.log(data);
+
+    //     client.broadcast.emit('enviarMensaje', data);
+
+
+    //     // if (mensaje.usuario) {
+    //     //     callback({
+    //     //         resp: 'TODO SALIO BIEN!'
+    //     //     });
+
+    //     // } else {
+    //     //     callback({
+    //     //         resp: 'TODO SALIO MAL!!!!!!!!'
+    //     //     });
+    //     // }
+
+
+
+    // });
+
+});
